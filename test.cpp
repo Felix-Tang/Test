7014f215ddcd47fe9d1d4388d9bdfd415354b9ef
Felix-Tang <felix_tang@163.com>
2013-08-22 17:45:11 +0800

    On branch master  Changes to be committed: 	new file:   test.cpp  Untracked files: 	StdAfx.cpp 	StdAfx.h

---
+++ test.cpp
@@ -0,0 +1,29 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "malloc.h"
+#include "string.h"
+
+void test(char** addr)
+{
+	*addr = (char*)malloc(10);
+
+	memset(*addr, 0 ,10);
+
+	memcpy(*addr,  "123456789", 9);
+}
+
+
+int main(int argc, char* argv[])
+{
+	char **temp;
+	char *temp1 = NULL;
+
+	test(&temp1);
+
+	printf("temp1 is %s \r\n", temp1);
+
+	return 0;
+}
