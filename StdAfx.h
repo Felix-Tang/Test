2885df508cc2ca9dc72febed4d9ff573dc82d079
Felix-Tang <felix_tang@163.com>
2013-08-22 18:03:33 +0800

    1111

---
+++ StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__41E43D17_28DB_4B56_9694_9FC2CC74687F__INCLUDED_)
+#define AFX_STDAFX_H__41E43D17_28DB_4B56_9694_9FC2CC74687F__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__41E43D17_28DB_4B56_9694_9FC2CC74687F__INCLUDED_)
